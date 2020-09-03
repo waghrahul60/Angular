50ed30c4019ddbfdcfb437abdeaa98c9c50aff03
waghrahul60 <waghrahul60@gmail.com>
2020-09-03 15:25:11 +0530

    loginRegister.

--- Project/rabne-banadi-jodi/src/app/app.module.ts
+++ Project/rabne-banadi-jodi/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MyhomeComponent } from './myhome/myhome.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, PageNotFoundComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, PageNotFoundComponent, MyhomeComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
