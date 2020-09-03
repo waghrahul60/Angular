f852fa33e3fffade5ed26329040f15c10568b850
waghrahul60 <waghrahul60@gmail.com>
2020-09-03 14:06:30 +0530

    pagesCreated

--- Project/rabne-banadi-jodi/src/app/app.module.ts
+++ Project/rabne-banadi-jodi/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
